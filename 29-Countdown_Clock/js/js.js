46f403283ca901682ffd1bddc20e79dfafdeff55
Sergiy <dmitruksl92@gmail.com>
2017-07-04 01:43:14 +0300

    Updated lessons  #29

--- 29-Countdown_Clock/js/js.js
+++ 29-Countdown_Clock/js/js.js
@@ -4,7 +4,7 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
-const buttons = document.querySelector('[data-time]');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
     // clear any existing timers
